c2386b61b607375c1c7dca8a96883bb044aa21a2
Daniela Negrut <maria.negrut@softwire.com>
2023-06-28 17:20:53 +0300

    Compute rook available moves

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,22 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves: Square[] = [];
+        const currentSquare: Square = board.findPiece(this);
+
+        for (let i = 0; i < 8 ; i++) {
+            const possibleMoveRow = new Square(i, currentSquare.col);
+            const possibleMoveColumn = new Square(currentSquare.row, i);
+
+            if (!possibleMoveRow.equals(currentSquare)) {
+                availableMoves.push(possibleMoveRow);
+            }
+
+            if (!possibleMoveColumn.equals(currentSquare)) {
+                availableMoves.push(possibleMoveColumn);
+            }
+        }
+
+        return availableMoves;
     }
 }
