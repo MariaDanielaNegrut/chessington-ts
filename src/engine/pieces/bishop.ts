06ba0a69fe402019fde9bbdfeafafccf3b28392b
Daniela Negrut <maria.negrut@softwire.com>
2023-06-28 17:36:26 +0300

    Compute bishop moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+        for (let i = 1; i < 8; i++) {
+            if (currentSquare.col - i >= 0 && currentSquare.row - i >= 0) {
+                availableMoves.push(new Square(currentSquare.row - i, currentSquare.col - i));
+            }
+
+            if (currentSquare.col + i < 8 && currentSquare.row + i < 8) {
+                availableMoves.push(new Square(currentSquare.row + i, currentSquare.col + i));
+            }
+
+            if (currentSquare.col + i < 8 && currentSquare.row - i >= 0) {
+                availableMoves.push(new Square(currentSquare.row - i, currentSquare.col + i));
+            }
+
+            if (currentSquare.col - i >= 0 && currentSquare.row + i < 8) {
+                availableMoves.push(new Square(currentSquare.row + i, currentSquare.col - i));
+            }
+        }
+        return availableMoves;
     }
 }
