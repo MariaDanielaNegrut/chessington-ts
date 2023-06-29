7f8398f6818a8892be9e3a5fb178215e151f2689
Daniela Negrut <maria.negrut@softwire.com>
2023-06-29 10:28:59 +0300

    Add knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,41 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+        if (currentSquare.col + 2 < 8 && currentSquare.row - 1 >= 0) {
+            availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col + 2));
+        }
+
+        if (currentSquare.col - 2 >= 0 && currentSquare.row - 1 >= 0) {
+            availableMoves.push(new Square(currentSquare.row - 1, currentSquare.col - 2));
+        }
+
+        if (currentSquare.col + 2 < 8 && currentSquare.row + 1 < 8) {
+            availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col + 2));
+        }
+
+        if (currentSquare.col - 2 >= 0 && currentSquare.row + 1 < 8) {
+            availableMoves.push(new Square(currentSquare.row + 1, currentSquare.col - 2));
+        }
+
+        if (currentSquare.col + 1 < 8 && currentSquare.row + 2 < 8) {
+            availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col + 1));
+        }
+
+        if (currentSquare.col - 1 >= 0 && currentSquare.row + 2 < 8) {
+            availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col - 1));
+        }
+
+        if (currentSquare.col + 1 < 8 && currentSquare.row - 2 >= 0) {
+            availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col + 1));
+        }
+
+        if (currentSquare.col - 1 >= 0 && currentSquare.row - 2 >= 0) {
+            availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col - 1));
+        }
+
+        return availableMoves;
     }
 }
