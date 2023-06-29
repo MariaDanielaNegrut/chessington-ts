07997a520ddfb1a961b246f9bc7137adddd9bbe4
Daniela Negrut <maria.negrut@softwire.com>
2023-06-29 10:35:29 +0300

    Add king moves

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (!(i === 0 && j === 0)) {
+                    availableMoves.push(Square.at(currentSquare.row + i, currentSquare.col + j));
+                }
+            }
+        }
+
+        return availableMoves;
     }
 }
